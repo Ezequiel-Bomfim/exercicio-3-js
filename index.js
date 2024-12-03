8705a93599601a1f041a1c1783be6194b365f6b2
Ezequiel Bomfim <ezequiel88er@gmail.com>
2024-12-03 09:10:43 -0300

    inserindo novo exercício

---
+++ index.js
@@ -0,0 +1,10 @@
+const aventureiroEscolhe = confirm("Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?")
+
+console.log(aventureiroEscolhe)
+
+
+if(aventureiroEscolhe){
+    alert("Ótimo! Nós temos as melhores camas de toda a região!")
+}else{
+    alert("Que pena! Você parecia ser uma pessoa mais legal")
+}
